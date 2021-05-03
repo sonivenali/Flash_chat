d07843b001cbd53f1ea08ec121bdb9006d332f2d
Venali Soni <sonivenali.vs@gmail.com>
2021-05-03 23:52:43 +0530

    Updating in ui

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -8,8 +8,16 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/image/logo.png",
+              height: 80,
+            ),
+          ],
+        ),
       ),
     );
   }
