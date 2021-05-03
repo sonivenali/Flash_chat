d988fa6ff5be3ccfc4d32d051b16a16884667d0e
Venali Soni <sonivenali.vs@gmail.com>
2021-05-03 13:32:31 +0530

    Created Login Screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flash_chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,6 +9,79 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/image/logo.png",
+              height: 140,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Enter your email",
+                  style: TextStyle(color: Colors.grey),
+                ),
+
+                minWidth: MediaQuery.of(context).size.height,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: Colors.lightBlueAccent,
+                        width: 1,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Enter your password",
+                  style: TextStyle(color: Colors.grey),
+                ),
+
+                minWidth: MediaQuery.of(context).size.height,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: Colors.lightBlueAccent,
+                        width: 1,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16,top: 30),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ChatScreen()),
+                  );
+                },
+                child: Text(
+                  "Log in",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.lightBlueAccent,
+                minWidth: MediaQuery.of(context).size.height,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: Colors.lightBlueAccent,
+                        width: 1,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
